fecbd1ff2527dbfdcded3417c9bba46f2067a3e8
Guilherme Bellotti <guibellotti@hotmail.com>
2020-06-05 23:59:05 -0300

    Add 'criar/listar ponto de coleta/items' e DB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
-import express, { response } from "express";
+import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
 
 const app = express();
-
-app.get("/users", (req, res) => {
-  console.log("Listagem de usuários");
-  res.json(["Guilherme", "Juninho"]);
-});
-
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 app.listen(3333);
